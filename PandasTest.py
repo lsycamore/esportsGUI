e1810cfca9291a34cd0f27115c81a0b296bf9e17
lsycamore <LS40548@suffolkone.ac.uk>
2019-04-29 10:26:54 +0100

    Test

---
+++ PandasTest.py
@@ -0,0 +1,8 @@
+import PySimpleGUI as sg
+import csv
+import os.path
+import pandas as pd
+import howdoi
+import sys
+df = pd.read_csv('teams.csv')
+    
--- main.py
+++ main.py
@@ -128,6 +128,8 @@ def AddResults():
 
     
         writer.writerow(fields)
+   # with open("teams.csv", "w+") as csvfile:
+
 #==============================================================================
 #def resultsMenu():
 
@@ -161,6 +163,34 @@ def CreateRankList():
 #==============================================================================
 #==============================================================================
 #==============================================================================
-
+def IndividualMenu():
+    layout = [
+                [sg.Text('Please select a function', auto_size_text=True)],  
+                [sg.Text('1. Create a team', auto_size_text=True)],
+                [sg.Text('2. Add results', auto_size_text=True)],
+                [sg.Text('3. View results', auto_size_text=True)],
+                [sg.Text('4. View ranked list', auto_size_text=True)],
+                [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('4'),sg.Quit()]]      
+    # Show the Window to the user    
+    window = sg.Window('Main Menu', no_titlebar=True).Layout(layout)      
+     # Event loop. Read buttons, make callbacks      
+    while True:      
+        # Read the Window    
+      event, value = window.Read()      
+        # Take appropriate action based on button      
+      if event == '1':      
+            CreateTeam()      
+      elif event == '2':      
+            AddResults()
+      elif event == '3':
+            ResultsMenu()
+      elif event == '4':
+            CreateRankList()
+      elif event =='Quit'  or event is None:  
+            window.Close()    
+            break      
+    
+        # All done!      
+    sg.PopupOK('Done')
 
 MainMenu()
